f7864088578fc72ed18f0afe360244d87ba28116
ssirai <shemhce@gmail.com>
2014-06-22 22:02:24 +0900

    add initial contents

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,29 +3,33 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'ssirai'
-SITENAME = 'ולם100'
+SITENAME = 'גולם壱佰'
 SITEURL = ''
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'Japan/Osaka'
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = 'ja'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+FEED_ALL_ATOM = None # 'feeds/all.rss.xml'
+CATEGORY_FEED_ATOM = None # 'feeds/%s.rss.xml'
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+LINKS =  (('Archive', 'archives.html'),)
+REVERSE_ARCHIVE_ORDER = 1
+DIRECT_TEMPLATES = ('index', 'tags', 'categories', 'archives')
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# SOCIAL = (('github', 'https://github.com/ssirai'))
 
 DEFAULT_PAGINATION = 10
 
+DEFAUT_DATE_FORMAT = '%Y %B %d'
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+ARTICLE_URL     = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
